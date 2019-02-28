90b393056755d72be8809aef4c9709427af22425
Nhat Trinh <goodyshoes_nt@yahoo.com>
2019-02-27 17:48:31 -0800

    added fields to models for auth api

--- server/auth_api/models.py
+++ server/auth_api/models.py
@@ -1,10 +1,26 @@
-from django.contrib.auth import models
-from django.contrib.postgres.fields import ArrayField
-from django.db import models as db_models
+from django.contrib.auth            import models
+from django.contrib.postgres.fields import ArrayField, CITextField 
+from django.db                      import models as db_models
+from django.db.models               import CharField, BooleanField, IntegerField
 import uuid
 
 class User(models.User):
-    # add more fields such as stock tickers
-    tickers = ArrayField(db_models.CharField(max_length=255, blank=True, default=''), default=list, null=True)
+    first_name      = CharField(max_length=30, null=False)
+    last_name       = CharField(max_length=30, null=False)
+    is_college      = BooleanField(default=False, null=False)
+    courses_taken   = ArrayField(CharField(max_length=30))
+    grade_level     = IntegerField(max_length=2, editable=True, null=False)
+
+class Tutor(User):
+    rating          = IntegerField(max_length=1, default=0)
+    old_rating      = IntegerField(max_length=0, default=0)
+    old_count       = IntegerField(max_length=10, default=0)
+
+class Course():
+    code                = CharField(max_length=30)
+    name                = CharField(max_length=30, null=False)
+    institution_name    = CharField(max_length=30, null=False)
+
+    
 
 
