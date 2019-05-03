e1e1bf67ef42df6459ac14d43b95cb06537d5143
Jennifer <july19.jenni@gmail.com>
2019-05-03 15:33:55 -0700

    change search background

--- client/src/Components/SearchPage.js
+++ client/src/Components/SearchPage.js
@@ -3,6 +3,9 @@ import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reac
 import { Redirect } from 'react-router-dom';
 import SearchForm from './SearchForm';
 import '../App.css';
+import {MDBContainer,
+        MDBRow,
+        MDBCol } from "mdbreact";
 
 
 
@@ -16,7 +19,17 @@ import '../App.css';
         return(
           <div>
             <div className="background-image" style={ styles.homeStyle }>
-              <SearchForm/>
+
+            <MDBContainer className="px-md-3 px-sm-0">
+                <MDBRow>
+                  <MDBCol md="12" className="mb-4 black-text text-center">
+                        <SearchForm/>
+                  </MDBCol>
+                </MDBRow>
+              </MDBContainer>
+
+
+
             </div>
 
           </div>
@@ -30,7 +43,16 @@ import '../App.css';
         alignItems: 'center',
         justifyContent: 'center',
         marginTop: '0px',
-        paddingTop: '30vh',
+        paddingTop: '20vh',
         overflowY: 'hidden'
-      }
+      },
+      hr:{
+          backgroundColor: '#d3a13b',
+          border: '3px solid #d3a13b',
+          borderRadius: '2px',
+          width: '50%',
+          textAlign: 'center',
+
+
+      },
     }
